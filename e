fc14beadf102b74d0350297fe628e741dbe21d33
codervivek <62784060+codervivek5@users.noreply.github.com>
2023-07-08 15:21:33 +0530

    bug fixed now it can redirect it any menu with corresping news

---
+++ e
@@ -0,0 +1,17 @@
+[33mcommit 9fd2bf34bda492131a63f431afa8e0c65b7b66eb[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: codervivek <62784060+codervivek5@users.noreply.github.com>
+Date:   Sat Jul 8 11:35:02 2023 +0530
+
+    try to solve bug for navigation bar
+
+[33mcommit d6cd16edfce16c9b604a48d9c62e73c5ab3465aa[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: codervivek <62784060+codervivek5@users.noreply.github.com>
+Date:   Wed Jul 5 19:09:59 2023 +0530
+
+    add redirect functionality
+
+[33mcommit bffc8157414202f27bde9ea410c7542646b60b73[m
+Author: codervivek <62784060+codervivek5@users.noreply.github.com>
+Date:   Wed Jul 5 10:06:10 2023 +0530
+
+    add all file first time
--- src/App.js
+++ src/App.js
@@ -12,14 +12,30 @@ export default class App extends Component {
           <NavBar />
 
           <Switch>
-            <Route path="/" element={<News pageSize={6} country="in" category="general" key="general" />} />
-            <Route path="/business" element={<News pageSize={6} country="in" category="business" key="business" />} />
-            <Route path="/entertainment" element={<News pageSize={6} country="in" category="entertainment" key="entertainment" />} />
-            <Route path="/general" element={<News pageSize={6} country="in" category="general" key="general" />} />
-            <Route path="/health" element={<News pageSize={6} country="in" category="health" key="health" />} />
-            <Route path="/science" element={<News pageSize={6} country="in" category="science" key="science" />} />
-            <Route path="/sports" element={<News pageSize={6} country="in" category="sports" key="sports" />} />
-            <Route path="/technology" element={<News pageSize={6} country="in" category="technology" key="technology" />} />
+            <Route path="/" exact>
+              <News pageSize={6} country="in" category="general" key="general" />
+            </Route>
+            <Route path="/business">
+              <News pageSize={6} country="in" category="business" key="business" />
+            </Route>
+            <Route path="/entertainment">
+              <News pageSize={6} country="in" category="entertainment" key="entertainment" />
+            </Route>
+            <Route path="/general">
+              <News pageSize={6} country="in" category="general" key="general" />
+            </Route>
+            <Route path="/health">
+              <News pageSize={6} country="in" category="health" key="health" />
+            </Route>
+            <Route path="/science">
+              <News pageSize={6} country="in" category="science" key="science" />
+            </Route>
+            <Route path="/sports">
+              <News pageSize={6} country="in" category="sports" key="sports" />
+            </Route>
+            <Route path="/technology">
+              <News pageSize={6} country="in" category="technology" key="technology" />
+            </Route>
           </Switch>
         </Router>
       </div>
